34bf6ff3776f009cc3eb800065e24c58bccc5731
Sheep Extraordinaire <76109159+rosehairedsheep@users.noreply.github.com>
2025-12-28 11:03:49 +0200

    okay i cleaned up the overlay system i think

--- Scripts/_Main/MainScene.cs
+++ Scripts/_Main/MainScene.cs
@@ -27,7 +27,7 @@ public partial class MainScene : Node2D, IConfigReliant
 				AddOverlay(SettingsScene.Instantiate<UILayer>());
 			}
 		}
-		else if (@Event.IsActionPressed("ToggleDebugOverlay"))
+		else if (@Event.IsActionPressed("ToggleDebug"))
 		{
 			if (Router.Config.FetchConfig<bool>("Debug", "DebugEnabled"))
 			{
@@ -39,7 +39,7 @@ public partial class MainScene : Node2D, IConfigReliant
 
 	public void OnConfigUpdate()
 	{
-		if (CurrentDebug != null && !Router.Config.FetchConfig<bool>("Debug", "DebugEnabled")) { ToggleDebug(); }
+		if (CurrentDebug != null && !Router.Config.FetchConfig<bool>("Debug", "DebugEnabled")) { CloseOverlay(CurrentDebug); }
 	}
 
 	#region Overlays
@@ -49,78 +49,23 @@ public partial class MainScene : Node2D, IConfigReliant
 
 	[Export] private Control OverlayHolder;
 
-	private Dictionary<int, List<UILayer>> ActiveOverlays = [];
+	private List<UILayer> ActiveOverlays = [];
 	private UILayer CurrentDebug = null;
 
-	public void AddOverlay(UILayer Overlay, int Layer = 0)
+	public void AddOverlay(UILayer Overlay, bool Debug = false)
 	{
-		Layer = Math.Clamp(Layer, 0, OverlayHolder.GetChildCount() - 1);
-		OverlayHolder.GetChild(Layer).AddChild(Overlay);
-
-		if (!ActiveOverlays.TryGetValue(Layer, out List<UILayer> Value))
-		{
-			Value = [];
-			ActiveOverlays[Layer] = Value;
-		}
-		Value.Add(Overlay);
+		OverlayHolder.GetChild(Debug ? 1 : 0).AddChild(Overlay);
+		if (!Debug) { ActiveOverlays.Add(Overlay); }
+		else { CurrentDebug = Overlay; }
 
 		SetTopOverlay();
 	}
 
-	public void CloseOverlay(UILayer Overlay)
-	{
-		if (!Overlay.RequestOverlayExit()) { return; }
-
-		foreach (int Key in ActiveOverlays.Keys)
-		{
-			if (ActiveOverlays[Key].Remove(Overlay))
-			{
-				Overlay.QueueFree();
-				SetTopOverlay();
-				return;
-			}
-		}
-	}
-
 	private UILayer GetTopOverlay()
 	{
-		List<int> SortedKeys = [.. ActiveOverlays.Keys];
-		SortedKeys.Sort();
-		SortedKeys.Reverse();
-
-		foreach (int Layer in SortedKeys)
-		{
-			if (ActiveOverlays[Layer].Count > 0)
-			{
-				return ActiveOverlays[Layer][^1];
-			}
-		}
+		if (CurrentDebug != null) { return CurrentDebug; }
+		if (ActiveOverlays.Count > 0) { return ActiveOverlays[^1]; }
 		return null;
-	}
-
-	private bool CloseTopOverlay()
-	{
-		List<int> SortedKeys = [.. ActiveOverlays.Keys];
-		SortedKeys.Sort();
-		SortedKeys.Reverse();
-
-		foreach (int Layer in SortedKeys)
-		{
-			if (ActiveOverlays[Layer].Count > 0)
-			{
-				if (ActiveOverlays[Layer][^1].RequestOverlayExit())
-				{
-					ActiveOverlays[Layer][^1].QueueFree();
-					ActiveOverlays[Layer].RemoveAt(ActiveOverlays[Layer].Count - 1);
-
-					// FIXME. Bad, bad, bad.
-					CurrentDebug = null;
-					SetTopOverlay();
-				}
-				return true;
-			}
-		}
-		return false;
 	}
 
 	private void SetTopOverlay()
@@ -128,16 +73,35 @@ public partial class MainScene : Node2D, IConfigReliant
 		GetTopOverlay()?.CallDeferred(UILayer.MethodName.GrabDefaultFocus);
 	}
 
+	private bool CloseTopOverlay()
+	{
+		UILayer TopOverlay = GetTopOverlay();
+		if (TopOverlay == null) { return false; }
+
+		CloseOverlay(TopOverlay);
+		return true;
+	}
+
+	public void CloseOverlay(UILayer Overlay)
+	{
+		if (!Overlay.RequestOverlayExit()) { return; }
+
+		if (Overlay == CurrentDebug) { CurrentDebug = null; }
+		else if (!ActiveOverlays.Remove(Overlay)) { return; }
+
+		Overlay.QueueFree();
+		SetTopOverlay();
+	}
+
 	private void ToggleDebug()
 	{
 		if (CurrentDebug != null)
 		{
 			CloseOverlay(CurrentDebug);
-			CurrentDebug = null;
 			return;
 		}
 		
-		AddOverlay(CurrentDebug = DebugScene.Instantiate<UILayer>(), 1);
+		AddOverlay(CurrentDebug = DebugScene.Instantiate<UILayer>(), true);
 	}
 	#endregion
 }
