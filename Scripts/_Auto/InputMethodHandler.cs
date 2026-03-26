fb2439f759c7b666ad7b4d56f6441f40420fecd8
Sheep Extraordinaire <76109159+rosehairedsheep@users.noreply.github.com>
2026-03-26 17:12:00 +0200

    input method buttons work

--- Scripts/_Auto/InputMethodHandler.cs
+++ Scripts/_Auto/InputMethodHandler.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class InputMethodHandler : Node
 {
@@ -8,12 +9,30 @@ public partial class InputMethodHandler : Node
 		Router.Input = this;
 	}
 
-	public enum INPUT_METHODS
-	{
-		Keyboard,
-		Joypad
-	}
+	public enum INPUT_METHODS { Keyboard, Joypad }
 	public INPUT_METHODS InputMethod = INPUT_METHODS.Keyboard; // TODO. Replace default.
+
+	public enum CONTROLLER_BUTTONS { BAXY, ABYX, CXST } // clockwise, starting from the right
+	public CONTROLLER_BUTTONS ControllerButtons = CONTROLLER_BUTTONS.BAXY; // TODO. Replace default.
+	public Dictionary<CONTROLLER_BUTTONS, InputButton[]> InputButtons = new()
+	{
+		{CONTROLLER_BUTTONS.BAXY, [
+			new InputButton(new Vector2(26, 18), new Color(208, 66, 66)),
+			new InputButton(new Vector2(18, 18), new Color(60, 219, 78)),
+			new InputButton(new Vector2(18, 24), new Color(64, 204, 208)),
+			new InputButton(new Vector2(26, 24), new Color(236, 219, 51))]},
+		{CONTROLLER_BUTTONS.ABYX, [
+			new InputButton(new Vector2(18, 18), new Color(191, 91, 91)),
+			new InputButton(new Vector2(26, 18), new Color(198, 185, 85)),
+			new InputButton(new Vector2(26, 24), new Color(134, 180, 96)),
+			new InputButton(new Vector2(18, 24), new Color(60, 141, 136))]},
+		{CONTROLLER_BUTTONS.CXST, [
+			new InputButton(new Vector2(42, 24), new Color(255, 102, 102)),
+			new InputButton(new Vector2(42, 18), new Color(124, 178, 232)),
+			new InputButton(new Vector2(34, 24), new Color(255, 105, 248)),
+			new InputButton(new Vector2(34, 18), new Color(64, 226, 160))]}
+	};
+
 	[Signal] public delegate void InputMethodChangedEventHandler(int NewMethod);
 	private const float AxisOffsetMin = 0.1f;
 
@@ -57,4 +76,10 @@ public partial class InputMethodHandler : Node
 		InputMethod = NewMethod;
 		EmitSignal(SignalName.InputMethodChanged, (int)InputMethod);
 	}
+}
+
+public class InputButton(Vector2 Pos, Color Col)
+{
+	public Vector2 IconPosition = Pos;
+	public Color ButtonColour = Col;
 }
